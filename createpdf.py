b4a9b2a6982148c0a4fc1b3374b1bde496f503c0
Kanike <prakashsai040@gmail.com>
2022-02-21 13:24:18 +0530

    Tried using aspose.words, built basic doc, Library isn't a open source, need to buy, so lettig go to other libraries

---
+++ createpdf.py
@@ -0,0 +1,18 @@
+# Creating a demp invoice structue using python libraries. 
+
+# import aspose.words as aw 
+
+# # create document object 
+# doc = aw.Document()
+
+# # create a document buiilder class
+# builder = aw.DocumentBuilder(doc)
+
+# #add text to the document
+# builder.write('Hello new docx')
+
+# #save doc
+# doc.save('InvoiceFiles\demo.docx')
+
+""" Above shown library is not for free. so moving to the python-docx"""
+
