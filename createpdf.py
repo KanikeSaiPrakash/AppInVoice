0189fa3c942d775daa1388456a0edda6c2133297
Kanike <prakashsai040@gmail.com>
2022-02-22 20:31:18 +0530

    Invoice aligned center and bolded, successfully

--- createpdf.py
+++ createpdf.py
@@ -16,3 +16,25 @@
 
 """ Above shown library is not for free. so moving to the python-docx"""
 
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+document = Document()
+for i in range(5):
+    document.add_paragraph(" ")
+
+ # adding invoice aligned at center.   
+invoice_para = document.add_paragraph("")
+run = invoice_para.add_run("INVOICE")
+invoice_para.alignment = 1
+run.bold = True
+# # or use the following ::::
+run.font.size = Pt(25)
+run.font.name = 'Times New Roman'
+
+
+
+
+document.save('InvoiceFiles\demo2.docx')
+
